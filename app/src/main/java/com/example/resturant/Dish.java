d62f316a32cded51618a06ef97369aba8564a592
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-01 23:02:22 +0300

    add @Entiti task class

--- app/src/main/java/com/example/resturant/Dish.java
+++ app/src/main/java/com/example/resturant/Dish.java
@@ -1,4 +1,27 @@
 package com.example.resturant;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Dish {
+
+   @PrimaryKey
+   public int taskId;
+
+   @ColumnInfo(name = "name")
+   public String name;
+
+   @ColumnInfo(name = "ingrediants")
+   public String ingrediants;
+
+   @ColumnInfo(name = "price")
+   public double price;
+
+   public Dish(String name, String ingrediants, double price) {
+      this.name = name;
+      this.ingrediants = ingrediants;
+      this.price = price;
+   }
 }
