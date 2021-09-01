e2a5ddf33cc561522d7f1e0cb7924831bd2ee09c
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-01 23:10:33 +0300

    add database class

--- app/src/main/java/com/example/resturant/DishDao.java
+++ app/src/main/java/com/example/resturant/DishDao.java
@@ -1,12 +1,14 @@
 package com.example.resturant;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
 import java.util.List;
 
-public interface TaskDao {
+@Dao
+public interface DishDao {
 
     @Query("SELECT * FROM dish")
     List<Dish> getAll();
